51c27c14dac44a54379fddf7ffb7326d0282bbf2
A7medhq <a.m.hq98@gmail.com>
2023-01-17 13:29:13 +0200

    mm

--- android/app/src/main/kotlin/com/example/biometrics/MainActivity.kt
+++
@@ -1,7 +0,0 @@
-package com.example.biometrics
-
-import io.flutter.embedding.android.FlutterFragmentActivity
-
-class MainActivity: FlutterFragmentActivity() {
-    // ...
-}
--- lib/main.dart
+++ lib/main.dart
@@ -41,7 +41,7 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[],
+          children: <Widget>[GestureDetector()],
         ),
       ),
       floatingActionButton: FloatingActionButton(
